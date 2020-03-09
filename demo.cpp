9e57bbcf16c463ed40cfbb14ac6a6686fd6d2a7d
emersondevtoo <emersonvolkov@gmail.com>
2020-03-08 21:40:00 -0500

    R2D51 Configurando vscode para desarrollar en c++. Esto es una locura, hoy día es muy facíl.

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// https://www.youtube.com/watch?v=-erXR6k9TeE&t=4893s
+
+int main(int argc, char const *argv[])
+{
+  cout << "Hola Mundo" << endl;
+  int x ;
+  cin >> x;
+
+  cout << x << endl;
+
+  /* code */
+  return 0;
+}
